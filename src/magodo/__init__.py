a60d8db38eeb156701ef1fc0403cea957b08e7f7
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-06-03 13:26:11 -0400

    Bump version: 0.10.0 → 0.10.1

--- src/magodo/__init__.py
+++ src/magodo/__init__.py
@@ -25,6 +25,6 @@ __all__ = [
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
