dcf35b02878f242986653485710f684f6524ab15
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-06-06 14:28:46 -0400

    test: Remove old, flakey tests

--- tests/data/__init__.py
+++
@@ -1,6 +0,0 @@
-"""The tests.data package."""
-
-from ._core import get_all_todo_paths
-
-
-__all__ = ["get_all_todo_paths"]
--- tests/test_magic.py
+++ tests/test_magic.py
@@ -2,14 +2,12 @@
 
 from __future__ import annotations
 
-from typing import List, Type
+from typing import List
 
 from pytest import mark
 
-from magodo import MagicTodoMixin, Todo
-from magodo.types import AbstractMagicTodo, TodoSpell
-
-from .shared import MOCK_TODO_KWARGS, MagicTodo, assert_todos_equal
+from magodo import MagicTodoMixin
+from magodo.types import TodoSpell
 
 
 params = mark.parametrize
@@ -33,59 +31,6 @@ class LineTodo(MagicTodoMixin):
     todo_spells: List[TodoSpell] = []
 
     ident: str = "LINE TODO"
-
-
-@params(
-    "line,magic_todo_type,pre_inner_todo,expected_inner_todo",
-    [
-        (
-            "x:1030 2022-01-12 Some done todo.",
-            MagicTodo,
-            Todo(
-                desc="x:1030 Some done todo.",
-                metadata={"x": "1030"},
-                done=True,
-            ),
-            Todo(
-                desc="Some done todo. | ctime:HHMM dtime:HHMM",
-                done=True,
-            ),
-        ),
-        (
-            "x:1030 2022-01-12 Some done todo with a @ctx.",
-            MagicTodo,
-            Todo(
-                desc="x:1030 Some done todo with a @ctx.",
-                contexts=("ctx",),
-                metadata={"x": "1030"},
-                done=True,
-            ),
-            Todo(
-                desc="Some done todo with a ctx. | @ctx ctime:HHMM dtime:HHMM",
-                contexts=("ctx",),
-                done=True,
-            ),
-        ),
-    ],
-)
-def test_magic_todo(
-    line: str,
-    magic_todo_type: Type[AbstractMagicTodo[Todo]],
-    pre_inner_todo: Todo,
-    expected_inner_todo: Todo,
-) -> None:
-    """Test the MagicTodo.from_line() function."""
-    from_line_mtodo = magic_todo_type.from_line(line).unwrap()
-    from_line_mtodo = from_line_mtodo.new(**MOCK_TODO_KWARGS)
-
-    pre_inner_todo = pre_inner_todo.new(**MOCK_TODO_KWARGS)
-    from_todo_mtodo = magic_todo_type(pre_inner_todo).new(**MOCK_TODO_KWARGS)
-    post_inner_todo = from_todo_mtodo.todo.new(**MOCK_TODO_KWARGS)
-    expected_inner_todo = expected_inner_todo.new(**MOCK_TODO_KWARGS)
-
-    assert_todos_equal(post_inner_todo, expected_inner_todo)
-
-    assert repr(from_line_mtodo) == repr(from_todo_mtodo)
 
 
 @params("line", ["foo bar baz", "test | foo bar baz"])
